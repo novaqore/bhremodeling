0e6d21ed6364aa3d2d3eac1dca80cac314120850
novaqoredev <dev@novaqore.tech>
2025-01-19 15:51:46 -0800

    update

--- app/page.js
+++ app/page.js
@@ -4,10 +4,8 @@ import Loading from "@/components/Loading/Loading"
 import Login from "@/components/Login/Login"
 import { useAuth } from "@/contexts/auth"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
 export default function Home() {
-  const router = useRouter()
   const { user, loading } = useAuth()
 
   if(loading) return <Loading />
